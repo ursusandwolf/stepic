eede310570fbd18d8c1fc48522e824dcdc331d03
ursusandwolf <129175187+ursusandwolf@users.noreply.github.com>
2023-04-01 16:07:55 +0300

    Create test.py

---
+++ words_with_position/test.py
@@ -0,0 +1,5 @@
+words = ['feel', 'graduate', 'movie', 'fashionable', 'bacon',
+         'drop', 'produce', 'acquisition', 'cheap', 'strength',
+         'master', 'perception', 'noise', 'strange', 'am']
+words_with_position = list([(v,k) for k,v in enumerate(words,1)])
+print(words_with_position)
